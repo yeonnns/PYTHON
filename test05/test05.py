abd1ed1af50752be1904b0a8db8062e03e5a4540
Soyeon Kim <100174595+yeonnns@users.noreply.github.com>
2022-08-01 23:27:18 +0900

    test05 update

--- test05/test05.py
+++ test05/test05.py
@@ -10,6 +10,8 @@ while gop < 10:
     gop += 1
 '''
 
+'''
+    1~9단 출력
 dan = 2;
 
 while dan < 10:
@@ -18,12 +20,22 @@ while dan < 10:
         print('{0}  x {1} = {2}'.format(dan, gop, dan * gop))
         gop += 1
     dan += 1
+print('-----------------------')
 
 '''
-while dan < 10:
-    print('{0}  x {1} = {2}'.format(dan, gop, dan * gop))
-    dan +=1
-'''
 
-
-
+dan = 2
+i = 0
+while i < 2:
+    gop = 1
+    while gop < 10:
+        j = 0
+        result = ''
+        while j < 4:
+            result += '{0}  x {1} = {2}\t\t'.format(dan + j, gop, dan * gop)
+            j += 1
+        gop +=1
+        print(result)
+    print()
+    dan = 6
+    i += 1
